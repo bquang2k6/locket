528e0e03a4fee96434da29f0dbfac57575315272
bquang2k6 <phambbquang30@gmail.com>
2025-09-26 16:02:49 +0700

    add ads

---
+++ src/components/UI/ads/ads.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+export default function Ads() {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.log("AdSense error:", e);
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-1905437736346974"
+      data-ad-slot="5128836736"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+}
--- src/pages/UILocket/ExtendPage/mainHomeScreen.jsx
+++ src/pages/UILocket/ExtendPage/mainHomeScreen.jsx
@@ -7,6 +7,7 @@ import SelectFiendsList from "./Container/SelectFriendsList";
 import HeaderAfterCapture from "./Header/HeaderAfterCapture";
 import HeaderBeforeCapture from "./Header/HeaderBeforeCapture";
 // import PopupNotification from "../../../components/UI/PopupNotification";
+import Ads from "../../../components/UI/ads/ads";
 
 const MainHomeScreen = () => {
   const { navigation, camera, useloading, post } = useApp();
@@ -47,6 +48,7 @@ const MainHomeScreen = () => {
         {selectedFile ? <SelectFiendsList /> : <HistoryArrow />}{" "}
         <canvas ref={canvasRef} className="hidden" />
         {/* <PopupNotification /> */}
+        <Ads />
       </div>
     </div>
   );
